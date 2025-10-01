5d261c4f29b1655c91e4df86a4f7f329c36740fb
cirnopog <irenetorjussen@hotmail.no>
2025-10-01 14:00:22 +0200

    model. js made

---
+++ AktivitetsApp/js/model.js
@@ -0,0 +1,73 @@
+const model = {
+    app: {
+        currentPage: "home",
+        currentUser: null,
+    },
+
+    viewState: {
+        homePage: {
+            search: "",
+        },
+        logInPage: {
+            name: "",
+            password: "",
+            search: ""
+        },
+        registrationPage: {
+            name: "",
+            password: "",
+            search: "",
+        },
+        activityPage:{
+            feedback: "",
+            starfeedback: "",
+            search: ""
+        },
+        categoryPage: {
+            selectedCategory: "",
+            search: ""
+        },
+        searchResultPage: {
+            search: ""
+        },
+        activityCreator:{
+                img: "",
+                activityName: "",
+                rating: "",
+                duration: "",
+                difficulty: "",
+                description: "",
+                tags: ["",""],
+                search: ""
+        }
+    },
+
+    data: {
+        activityData:{
+                img: "",
+                activityName: "",
+                rating: "",
+                duration: "",
+                difficulty: "",
+                description: "",
+                tags: ["",""],
+        },
+        popularActivities: [
+
+        ],
+        categoryActivities: [
+
+        ],
+        searchResults: [
+            
+        ],
+        profileInfo: {
+            name: "",
+            img: "",
+            bio: "",
+            topActivities: [],
+            following: [],
+        }
+
+    }
+}
