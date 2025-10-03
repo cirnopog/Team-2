ba4770fba63fc16d3c17db5f9176ca527aba5f13
thunesti <thunestian@gmail.com>
2025-10-03 11:26:36 +0200

    Updated

---
+++ AktivitetsApp/js/CategoryPage/view.js
@@ -0,0 +1,12 @@
+categoriesView()
+
+function categoriesView() {
+    document.getElementById('categoriesHtml').innerHTML = /*HTML*/`
+    <div id="selectCategory">
+        <h3>Velg kategori</h3>
+    </div>
+    <div id="activityList">
+        <h3>Aktivitets-liste</h3>
+    </div>
+    `;
+}
