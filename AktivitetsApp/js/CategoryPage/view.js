d50f3c2a63f7114b16870901997dc2c919e40db0
thunesti <thunestian@gmail.com>
2025-10-03 14:27:38 +0200

    Updated

--- AktivitetsApp/js/CategoryPage/view.js
+++ AktivitetsApp/js/CategoryPage/view.js
@@ -2,11 +2,16 @@ categoriesView()
 
 function categoriesView() {
     document.getElementById('categoriesHtml').innerHTML = /*HTML*/`
-    <div id="selectCategory">
-        <h3>Velg kategori</h3>
+    <div id="heading-1">
+        <h3>Kategorier</h3>
     </div>
-    <div id="activityList">
-        <h3>Aktivitets-liste</h3>
+    <div id="submenu">
+        <p><< Back | Filter: [None] | Sort: [Rating] |</p>
+    </div>
+    <div id="container">
+        <p id="subheading">Liste over kategorier:</p>
+        <br>
+        <p>Her skal du kunne trykke på kategorier. Deretter vil tilhørende aktiviteter vises her. Da vil 'Liste over kategorier' endres til 'Liste over aktiviteter'. For å komme tilbake til kategorier bruker man kategorier-boksens meny. Der skal du også kunne filtrere og sortere hva du ser.</p>
     </div>
     `;
 }
