1746ab77b737dd85cccec811105cb597c97d7bfc
cirnopog <irenetorjussen@hotmail.no>
2025-10-03 14:08:50 +0200

    activity updates

--- AktivitetsApp/js/ActivityPage/view.js
+++ AktivitetsApp/js/ActivityPage/view.js
@@ -3,14 +3,18 @@ function activityView(i = 0){
     <div class="activity-info">
     <h2>${model.data.activities[i].name}</h2>
 
-    <div class="rating">
-        ${ratingStars(model.data.activities[i].rating)}
+    <div class="activity-stats">
+        <p class="rating">
+            ${ratingStars(model.data.activities[i].rating)}
+        </p>
+
+        <p class="difficulty">
+            Vanskelighet: ${model.data.activities[i].difficulty}
+        </p>
+        <p class="duration">
+            🕐${model.data.popularActivities[i].duration}        
+        </p>
     </div>
-
-    <p class="difficulty">
-       Vanskelighet: ${model.data.activities[i].difficulty}
-    </p>
-
     <div class="description">
         ${model.data.activities[i].description}
     </div>
@@ -21,9 +25,9 @@ function activityView(i = 0){
         <img src="${model.data.activities[i].img}" width="700" alt=""></img>
         <div class="tags">
                 ${createTags(i)}
-              
+              <i class="fa-regular fa-heart"></i>
         </div>
-        <i class="fa-regular fa-heart"></i>
+        
         
     </div>
     
