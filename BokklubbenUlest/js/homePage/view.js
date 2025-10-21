315c4dec1c5d2aea5d6884404d37296c10eace44
martinius97 <martiniusr@outlook.com>
2025-10-21 17:16:02 +0200

    Vote Funksjon

--- BokklubbenUlest/js/homePage/view.js
+++ BokklubbenUlest/js/homePage/view.js
@@ -1,29 +1,44 @@
-//Voting polls
-var bookList = document.getElementsById("booklist");
+// Bøker i avstemning
+function renderBooksInVoting() {
+    var bookList = document.getElementById("bookListContainer"); 
+    if (!bookList) return; 
+    bookList.innerHTML = ""; 
 
+    var books = model.data.booksInVoting;
 
+    for (var i = 0; i < books.length; i++) {
+        var book = books[i];
 
-
-
-
-
-
-function  vote(bookId, value){
-    for (var i = 0; i < model.booksInVoting.length; i++){
-        if (model.booksInVoting[i].id === bookId) {
-            model.booksInVoting[i].votes += value;
-            document.getElementById("votes"+ bookId).textContent = model.booksInVoting[i].votes;
-            break;
-
-            
-
-        }
         
+        if (book.votes === "") book.votes = 0;
+        book.votes = Number(book.votes);
+
+        var div = document.createElement("div");
+        div.id = "book" + book.id;
+        div.innerHTML = `
+            <div style="margin-bottom:10px;">
+                ${book.img ? `<img src="${book.img}" alt="${book.title}" style="width:50px; height:70px;">` : ""}
+                <strong>${book.title}</strong>
+                <button onclick="vote(${book.id},1)">👍</button>
+                <span id="votes${book.id}">${book.votes}</span>
+                <button onclick="vote(${book.id},-1)">👎</button>
+            </div>
+        `;
+        bookList.appendChild(div);
     }
 }
-/* 
-Finn ut hvilke bok som skal endres med ID
-Dette kan gjøres med parameter
-Etter det velg denne boken fra listen
-Pluss poeng med 1
-*/
+
+// stemme funksjon
+function vote(bookId, value) {
+    var books = model.data.booksInVoting;
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].id === bookId) {
+            books[i].votes = Number(books[i].votes) + value;
+            document.getElementById("votes" + bookId).textContent = books[i].votes;
+            break;
+        }
+    }
+}
+
+
+renderBooksInVoting();
