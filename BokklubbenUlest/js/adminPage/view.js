13e9d18ff672f3a2b8d3871294b9f2c7444acef7
thunesti <thunestian@gmail.com>
2025-10-22 13:09:28 +0200

    Update view.js

--- BokklubbenUlest/js/adminPage/view.js
+++ BokklubbenUlest/js/adminPage/view.js
@@ -4,16 +4,16 @@ userRegistrationPage();
 
 function userRegistrationPage() {
     document.getElementById('app').innerHTML += /*HTML*/`
-            <form id="userRegistrationForm" onsubmit="registerNewUser(event)">
-                <h3>Registrer ny bruker</h3>
-                <label for="name">Navn:</label>
-                <input type="text" id="nameOfUser" name="name" oninput="model.viewState.userRegistrationPage.name = this.value" required>
-                <label for="userPassword">Passord:</label>
-                <p>Minimum 10 tegn og minst en stor bokstav, en liten bokstav, et symbol og et tall.</p>
-                <input type="text" id="userPassword" name="password" oninput="model.viewState.userRegistrationPage.password = this.value" required minlength="10">
-                <button class="registerUserButton" type="submit">Registrer</button>
-                <p id="password-error" style="color: red;"></p>
-            </form>
+        <form id="userRegistrationForm" onsubmit="registerNewUser(event)">
+            <h3>Registrer ny bruker</h3>
+            <label for="name">Navn:</label>
+            <input type="text" id="nameOfUser" name="name" oninput="model.viewState.userRegistrationPage.name = this.value" required>
+            <label for="userPassword">Passord:</label>
+            <p>Minimum 10 tegn og minst en stor bokstav, en liten bokstav, et symbol og et tall.</p>
+            <input type="text" id="userPassword" name="password" oninput="model.viewState.userRegistrationPage.password = this.value" required minlength="10">
+            <button class="registerUserButton" type="submit">Registrer</button>
+            <p id="password-error" style="color: red;"></p>
+        </form>
     `;
 }
 
