fd11e43c248556559ef0d51206f948668385e489
thunesti <thunestian@gmail.com>
2025-10-22 13:04:40 +0200

    Updated admin functions

--- BokklubbenUlest/js/adminPage/view.js
+++ BokklubbenUlest/js/adminPage/view.js
@@ -1,20 +1,19 @@
 // REGISTRATION
+
+userRegistrationPage();
+
 function userRegistrationPage() {
     document.getElementById('app').innerHTML += /*HTML*/`
-        <div class="userRegFormDiv">
-            <h3>Registrer ny bruker</h3>
-            <form id="userRegistrationForm">
+            <form id="userRegistrationForm" onsubmit="registerNewUser(event)">
+                <h3>Registrer ny bruker</h3>
                 <label for="name">Navn:</label>
-                <br>
                 <input type="text" id="nameOfUser" name="name" oninput="model.viewState.userRegistrationPage.name = this.value" required>
-                <br>
                 <label for="userPassword">Passord:</label>
-                <br>
-                <input type="text" id="userPassword" name="password" oninput="model.viewState.userRegistrationPage.password = this.value" required minlength="10" placeholder="Minimum 10 tegn">
-                <br>
-                <button class="registerUserButton" type="submit" onclick="registerNewUser()">Registrer</button>
+                <p>Minimum 10 tegn og minst en stor bokstav, en liten bokstav, et symbol og et tall.</p>
+                <input type="text" id="userPassword" name="password" oninput="model.viewState.userRegistrationPage.password = this.value" required minlength="10">
+                <button class="registerUserButton" type="submit">Registrer</button>
+                <p id="password-error" style="color: red;"></p>
             </form>
-        </div>
     `;
 }
 
