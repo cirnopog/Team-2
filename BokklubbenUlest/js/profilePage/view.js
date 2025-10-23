8a965e7945267e44357933f96ed50972a9bff0bc
FelixBerget <felixberget@gmail.com>
2025-10-23 10:26:11 +0200

    Added Profile Page

---
+++ BokklubbenUlest/js/profilePage/view.js
@@ -0,0 +1,19 @@
+function generateOtherUserProfile(){
+    let favorites = `<ul id="favs">`
+    let readBooks = `<ul id="readbooks">`
+    for(i in model.data.users[0].addedBooks){
+        readBooks+=model.data.users[0].addedBooks[i]
+    }
+    readBooks+=`</ul>`
+    for(i in model.data.users[0].favorites){
+        favorites+=model.data.users[0].favorites[i]
+    }
+    favorites+=`</ul>`
+    const app = document.getElementById("app")
+    app.innerHTML=`
+    <h1>${model.data.users[0].name}</h1>
+    <img src="img/${model.data.users[0].avatar}" alt="${model.data.users[0].avatar}" 20%" width="20%">
+    ${readBooks}
+    ${favorites}
+    `
+}
