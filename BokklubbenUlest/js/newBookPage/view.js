5421adbe854de5a6c8239d0d2abce8190de388b8
cirnopog <irenetorjussen@hotmail.no>
2025-10-27 09:37:56 +0100

    new book page update

--- BokklubbenUlest/js/newBookPage/view.js
+++ BokklubbenUlest/js/newBookPage/view.js
@@ -9,7 +9,10 @@ function newBookPage(){
         <div class="container"> 
             <div>
             <label for="bookTitle">Tittel</label>
-            <input type="text" id="bookTitle">
+            <input 
+                type="text" 
+                oninput="model.viewState.bookRegistrationPage.title = this.value"
+                >
             </div>
 
             <div>
@@ -18,15 +21,18 @@ function newBookPage(){
             </div>
             
             <div>
-            <label>Cover bilde</label>
-            <input type="text">
-            </div>
-
-            <div>
             <label>Kjøpslink</label>
             <input type="text">
             </div>
 
+            <div>
+            <label>Cover bilde</label>
+            <input 
+                type="file"
+                accept="image/*"
+            >
+            </div>
+            
             <div class="description-field">
             <label>Beskrivelse</label>
             <textarea name="" id="" cols="30" rows="10"></textarea>
